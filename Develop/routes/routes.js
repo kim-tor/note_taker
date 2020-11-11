12904485f335bf3ae6877a09f22a05f6a4d5c6af
Kim Torres <kimberley.5118@gmail.com>
2020-11-11 17:29:19 -0500

    updated Readme file, tried to make corrections in route js for post / delete

--- Develop/routes/routes.js
+++ Develop/routes/routes.js
@@ -3,9 +3,6 @@ const path = require("path");
 
 module.exports = app => {
 
-    // Grab the data from the db.json
-
-
     // Read the db.json file
     app.get("/api/notes", function (req, res) {
         fs.readFile("db/db.json", "utf8", (err, data) => {
@@ -17,41 +14,41 @@ module.exports = app => {
         });
     });
 
-        // Route to view notes.html
-        app.get('/notes', function (req, res) {
-            res.sendFile(path.join(__dirname, "../public/notes.html"));
+    // Route to view notes.html
+    app.get('/notes', function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+    
+    // ROUTES
+
+    app.post("/api/notes", (req, res) => {
+        let newNote = req.body;
+        notes.push(newNote);
+        updateDB();
+    });
+
+    // Retrieve a specific note
+    app.get("/api/notes/:id", (req, res) => {
+        res.json(notes[req.params.id]);
+    });
+
+    // Delete a specific note
+    app.delete("/api/notes/:id", (req, res) => {
+        notes.splice(req.params.id, 1);
+        updateDB();
+        console.log("Deleted note with id " + req.params.id);
+    });
+
+    function updateDB() {
+        fs.writeFile("db/db.json", JSON.stringify(notes, "\t"), err => {
+            if (err) throw err;
+            return true;
         });
+    };
 
-        function updateDB() {
-            fs.writeFile("db/db.json", JSON.stringify(notes, "\t"), err => {
-                if (err) throw err;
-                return true;
-            });
-        };
-
-        // ROUTES
-
-        app.post("/api/notes", (req, res) => {
-            let newNote = req.body;
-            notes.push(newNote);
-            updateDB();
-        });
-
-        // Retrieve a specific note
-        app.get("/api/notes/:id", (req, res) => {
-            res.json(notes[req.params.id]);
-        });
-
-        // Delete a specific note
-        app.delete("/api/notes/:id", (req, res) => {
-            notes.splice(req.params.id, 1);
-            updateDB();
-            console.log("Deleted note with id " + req.params.id);
-        });
-
-        // Route to view index.html 
-        app.get("*", (req, res) => {
-            res.sendFile(path.join(__dirname, "../public/index.html"));
-        });
+    // Route to view index.html 
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
 
 };
--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # note_taker
+
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#License)
+
+## Description
 Create an application that can be used to write, save, and delete notes.
+
+
+## Installation
+
+To begin you will need to run an npm install inquirer. 
+
+## Usage
+* I was able to load the page locally but I was unable to load it using heroku. I tried using the guide but I repeatedly got errors. <img src = cmd.jpg>.
+
+When I loaded the app locally I was able to see the landing page of the note taker and I could get to the screen where I could add a note. I was unable to save the notes / delete the notes. I think there may have been issues with the routes off my post and delete functions.
+
+## Resources
+* npmjs
+* expressjs
+* https://docs.github.com/en/free-pro-team@latest/github/using-git/ignoring-files
+* Met with TA to go over some questions as well
+
+## License
+MIT License [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+Copyright (c) [2020] [Kimberley Torres]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
